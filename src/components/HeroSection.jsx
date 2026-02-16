57e932a8af0f4bd39b4f20e58743621f01d77bd8
Samstickz <sadeleye437@gmail.com>
2026-02-16 02:56:54 +0100

    Use banner as profile temp

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import profileImg from "../profile.jpg";
+import profileImg from "../assets/banner.jpg";
 import bannerImg from "../assets/banner.jpg";
 import SpotifyWidget from "./SpotifyWidget";
 

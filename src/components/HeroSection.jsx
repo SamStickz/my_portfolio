82dcb1c2d653d06d8391cc00ac1841229c5625a7
Samstickz <sadeleye437@gmail.com>
2026-02-16 03:00:35 +0100

    Add profile image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import profileImg from "../assets/banner.jpg";
+import profileImg from "../assets/profile.jpg";
 import bannerImg from "../assets/banner.jpg";
 import SpotifyWidget from "./SpotifyWidget";
 
